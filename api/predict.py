553a4542d8eae75e1bc491937d141c741ca00298
giraldiego <giraldiego@gmail.com>
2024-01-08 16:12:31 -0500

    Add code to predict single patient in json format

--- api/predict.py
+++ api/predict.py
@@ -1,21 +1,22 @@
 import pickle
 from flask import Flask, request, jsonify
 
-input_file = "hospitalization-logistic_model.bin"
+input_file = "hospitalization-logistic_model_test.bin"
 
 with open(input_file, "rb") as f_in:
     dv, model = pickle.load(f_in)
 
 app = Flask("hospitalization")
 
-
 @app.route("/predict", methods=["POST"])
 def predict():
     patient = request.get_json()
 
-    X = dv.transform([patient])
-    y_pred = model.predict_proba(X)[0, 1]
-    hospitalization = y_pred >= 0.5
+    print(patient)
+
+    # X = dv.transform([patient])
+    # y_pred = model.predict_proba(X)[0, 1]
+    # hospitalization = y_pred >= 0.5
 
     result = {
         "hospitalization_probability" : float(y_pred),
